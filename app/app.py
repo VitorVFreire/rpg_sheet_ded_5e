2b290766558f7db4e5772a93eda4f27176697d40
Vitor Vieira Freire <vitorvfreire@outlook.com>
2023-06-30 21:28:26 -0300

    Session e logout

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,session
+from flask_session import Session
 import os
 from dotenv import load_dotenv
 
@@ -6,6 +7,9 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 from routes import *
 
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request,redirect, session, flash, url_for,jsonify
+from flask_session import Session
 
 from app import app
 from src import Usuario,Personagem
@@ -9,12 +10,20 @@ def index():
     
 @app.route('/login')
 def login():
+    if session.get('id_usuario'):
+        return render_template('index.html',titulo='home')
     return render_template('login.html',titulo='login')
+
+@app.route('/logout')
+def logout():
+    session['id_usuario'] = None
+    return redirect(url_for('index'))
 
 @app.post('/login')
 def cadastro_login():
     usuario=Usuario(email=request.form.get('email'),senha=request.form.get('senha'))
     if usuario.valid_usuario():
+        session['id_usuario']=usuario.id
         return render_template('index.html',titulo='home',msg='logado')
     return redirect(url_for('login'))
 
